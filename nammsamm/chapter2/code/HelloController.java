43c04d58cf2c247e59a5883d5c60fa217a8fb17e
nammsamm <108855492+nammsamm@users.noreply.github.com>
2023-03-24 01:16:09 +0900

    [2장] 스프링 부트에서 테스트 코드를 작성하자

--- nammsamm/chapter2/code/HelloController.java
+++ nammsamm/chapter2/code/HelloController.java
@@ -1,6 +1,7 @@
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController// 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 줌.
 public class HelloController {
     @GetMapping("/hello")//HTTP Method인 GET의 요청을 받을 수 있는 API생성
