b5b961b26f08938cf1a07c827bf2cc076c001542
Haeun Noh <00blowup@gmail.com>
2023-03-31 22:01:21 +0900

    [3장] 스프링 부트에서 JPA로 데이터베이스 다뤄보자

---
+++ 00blowup/freelec-springboot-webservice/src/main/java/org/example/springboot/domain/posts/PostsService.java
@@ -0,0 +1,38 @@
+package org.example.springboot.domain.posts;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springboot.web.dto.PostsResponseDto;
+import org.example.springboot.web.dto.PostsSaveRequestDto;
+import org.example.springboot.web.dto.PostsUpdateRequestDto;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        // orElseThrow는 해당 ID를 가진 데이터가 존재하지 않을 경우를 대비한 것임. Optional과 같은 역할인가?
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
