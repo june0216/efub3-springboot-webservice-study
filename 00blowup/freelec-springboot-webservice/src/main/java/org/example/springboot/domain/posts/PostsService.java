55621fbbcec04266d261d904122fc2536dd76a55
Haeun Noh <00blowup@gmail.com>
2023-04-07 04:13:17 +0900

    [4장] 머스테치로 화면 구성하기 - 전체 목록 조회 완성

--- 00blowup/freelec-springboot-webservice/src/main/java/org/example/springboot/domain/posts/PostsService.java
+++ 00blowup/freelec-springboot-webservice/src/main/java/org/example/springboot/domain/posts/PostsService.java
@@ -1,12 +1,16 @@
 package org.example.springboot.domain.posts;
 
 import lombok.RequiredArgsConstructor;
+import org.example.springboot.web.dto.PostsListResponseDto;
 import org.example.springboot.web.dto.PostsResponseDto;
 import org.example.springboot.web.dto.PostsSaveRequestDto;
 import org.example.springboot.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +38,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- 00blowup/freelec-springboot-webservice/src/main/java/org/example/springboot/web/IndexController.java
+++ 00blowup/freelec-springboot-webservice/src/main/java/org/example/springboot/web/IndexController.java
@@ -1,13 +1,24 @@
 package org.example.springboot.web;
 
+import org.example.springboot.domain.posts.PostsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
+    // 책에 있는 대로 위 한 줄만 적었더니 오류가 발생하여 아래의 constructor를 추가하였음
+    public IndexController(PostsService postsService) {
+        this.postsService = postsService;
+    }
+
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
