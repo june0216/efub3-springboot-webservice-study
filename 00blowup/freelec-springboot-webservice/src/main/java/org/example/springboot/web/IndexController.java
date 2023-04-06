db1fa9bc3c283961cafda9c6431152e7c965ae11
Haeun Noh <00blowup@gmail.com>
2023-04-07 04:32:21 +0900

    [4장] 머스테치로 화면 구성하기 - 게시글 수정 완성

--- 00blowup/freelec-springboot-webservice/src/main/java/org/example/springboot/web/IndexController.java
+++ 00blowup/freelec-springboot-webservice/src/main/java/org/example/springboot/web/IndexController.java
@@ -1,9 +1,11 @@
 package org.example.springboot.web;
 
 import org.example.springboot.domain.posts.PostsService;
+import org.example.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
@@ -25,5 +27,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate (@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
--- 00blowup/freelec-springboot-webservice/src/main/resources/static/js/app/index.js
+++ 00blowup/freelec-springboot-webservice/src/main/resources/static/js/app/index.js
@@ -4,6 +4,9 @@ var main = {
         $('#btn-save').on('click', function() {
             _this.save();
         });
+        $('#btn-update').on('click', function() {
+            _this.update();
+        })
     },
     save : function () {
         var data = {
@@ -23,6 +26,26 @@ var main = {
             window.location.href = '/'; // 시작 화면으로 이동
         }).fail(function(error) {   // 오류가 발생했을 경우
             alert(JSON.stringify(error));   //화면에 오류 알림을 띄움
+        });
+    },
+    update : function () {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=UTF-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function() {
+            alert(JSON.stringify(error));
         });
     }
 }
