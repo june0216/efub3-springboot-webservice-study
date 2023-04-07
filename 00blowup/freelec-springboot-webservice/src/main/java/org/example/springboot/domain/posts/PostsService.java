041ba8b4b1d8079f72ffd394834e5308ccdac1f3
Haeun Noh <00blowup@gmail.com>
2023-04-07 23:10:56 +0900

    [5장] 스프링 시큐리티와 OAuth 2.0으로 로그인 기능 구현하기 - 구글 로그인 인증정보 발급 완료

---
+++ 00blowup/freelec-springboot-webservice/src/main/java/org/example/springboot/domain/posts/PostsService.java
@@ -0,0 +1,57 @@
+package org.example.springboot.domain.posts;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springboot.web.dto.PostsListResponseDto;
+import org.example.springboot.web.dto.PostsResponseDto;
+import org.example.springboot.web.dto.PostsSaveRequestDto;
+import org.example.springboot.web.dto.PostsUpdateRequestDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        // orElseThrow는 해당 ID를 가진 데이터가 존재하지 않을 경우를 대비한 것임. Optional과 같은 역할인가?
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        postsRepository.delete(posts);
+    }
+}
